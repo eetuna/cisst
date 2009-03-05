6133c16980157a2217b6af108f70c24246b30bfc
Min Yang Jung <myj@jhu.edu>
2009-03-05 03:14:59 +0000

    updated mtsCollector
    git-svn-id: https://svn.lcsr.jhu.edu/cisst@115 0959eb1a-6956-4f60-947b-6092a4e22c97

--- branches/branches/2009-02-25_data_collection/libs/code/cisstMultiTask/mtsCollector.cpp
+++ branches/branches/2009-02-25_data_collection/libs/code/cisstMultiTask/mtsCollector.cpp
@@ -1,19 +1,40 @@
 /*
+  $Id: mtsCollector.cpp 2009-03-02 mjung5
+
   Author(s):  Min Yang Jung
   Created on: 2009-02-25
+
+  (C) Copyright 2008-2009 Johns Hopkins University (JHU), All Rights
+  Reserved.
+
+--- begin cisst license - do not edit ---
+
+This software is provided "as is" under an open source license, with
+no warranty.  The complete license can be found in license.txt and
+http://www.cisst.org/cisst/license.txt.
+
+--- end cisst license ---
+
 */
 
 #include <cisstMultiTask/mtsCollector.h>
 
+CMN_IMPLEMENT_SERVICES(mtsCollector)
+
 unsigned int mtsCollector::CollectorCount = 0;
+mtsTaskManager * mtsCollector::taskManager = NULL;
 
 //-------------------------------------------------------
 //	Constructor, Destructor, and Initializer
 //-------------------------------------------------------
-mtsCollector::mtsCollector(const std::string & collectorName, double period) : 
+mtsCollector::mtsCollector(const std::string & collectorName, double period) : 	
 	mtsTaskPeriodic(collectorName, period, false)
 {
 	++CollectorCount;
+
+	if (taskManager == NULL) {
+		taskManager = mtsTaskManager::GetInstance();
+	}
 }
 
 mtsCollector::~mtsCollector()
@@ -24,8 +45,8 @@ mtsCollector::~mtsCollector()
 }
 
 void mtsCollector::Init()
-{
-	//
+{	
+	SignalCollection.clear();		
 }
 
 //-------------------------------------------------------
@@ -40,9 +61,8 @@ void mtsCollector::Startup(void)
 void mtsCollector::Run(void)
 {
 	// periodic execution
-
+	// for test 
 	static int i = 0;
-
 	CMN_LOG_CLASS(5) << "----- [ " << GetName() << " ] " << ++i << std::endl;
 }
 
@@ -52,11 +72,150 @@ void mtsCollector::Cleanup(void)
 }
 
 //-------------------------------------------------------
-//	Signal Registration
+//	Signal Management
 //-------------------------------------------------------
 bool mtsCollector::AddSignal(const std::string & taskName, 
 							 const std::string & signalName, 
 							 const std::string & format)
-{
+{	
+	CMN_ASSERT(taskManager);
+
+	// Ensure that the specified task is under the control of the task manager.
+	//
+	//	TODO: Should the task be an instance of mtsTaskPeriodic???
+	//
+	mtsTask * task = taskManager->GetTask(taskName);
+	if (task == NULL) {
+		CMN_LOG_CLASS(5) << "Unknown task: " << taskName << std::endl;
+		return false;
+	}
+
+	// Prevent duplicate signal registration
+	if (FindSignal(taskName, signalName)) {
+		CMN_LOG_CLASS(5) << "Already registered signal: " << taskName 
+			<< ", " << signalName << std::endl;
+		return false;
+	}
+
+	// Check if the specified signal has been added to the state table.
+	//if (task->FindStateVectorDataName(signalName)) {
+	//	//
+	//	// TODO: Throw an exception if already collecting
+	//	// TODO: test unit: how to test throwing an exception? (add a test unit)
+	//	//
+	//	CMN_LOG_CLASS(5) << "Already registered signal: " << signalName << std::endl;
+	//	return false;
+	//}
+	
+	// Add a signal
+	SignalCollection.insert(make_pair(taskName, signalName));
+	CMN_LOG_CLASS(5) << "Signal added: " << taskName << ", " << signalName << std::endl;
+
+	// Adaptively control a period of the collector task in order to be sure that 
+	// the collector task has always the minimum period among all tasks added.
+
 	return true;
+}
+
+bool mtsCollector::RemoveSignal(const std::string & taskName, const std::string & signalName)
+{
+	// If no signal data is being collected.
+	if (SignalCollection.empty()) {
+		CMN_LOG_CLASS(5) << "No signal data is being collected." << std::endl;
+		return false;
+	}
+
+	// If finding a nonregistered task or signal
+	if (!FindSignal(taskName, signalName)) {
+		CMN_LOG_CLASS(5) << "Unknown task and/or signal: " << taskName 
+			<< ", " << signalName << std::endl;
+		return false;
+	}
+
+	// Remove a signal from the list
+	SignalMap::const_iterator itr = SignalCollection.find(taskName);
+	CMN_ASSERT(itr != SignalCollection.end());
+
+	// The specified task is found. Now try to find a signal with the given signal name
+	SignalMap::const_iterator itrOnePastTheLast;
+	itrOnePastTheLast = SignalCollection.upper_bound(taskName);
+	for (; itr != itrOnePastTheLast; ++itr) {
+		if (itr->second == signalName) {
+			SignalCollection.erase(itr);
+			CMN_LOG_CLASS(5) << "Signal removed: " << taskName 
+				<< ", " << signalName << std::endl;
+
+			return true;
+		}
+	}
+	
+	// This should not occur.
+	CMN_ASSERT(false);
+
+	return false;
+}
+
+bool mtsCollector::FindSignal(const std::string & taskName, const std::string & signalName)
+{
+	if (SignalCollection.empty()) return false;
+
+	// First, try to find a signal with the given task name
+	SignalMap::const_iterator itr = SignalCollection.find(taskName);
+	if (itr == SignalCollection.end()) {
+		return false;	// no task found
+	}
+
+	// The specified task is found. Now try to find a signal with the given signal name
+	SignalMap::const_iterator itrOnePastTheLast;
+	itrOnePastTheLast = SignalCollection.upper_bound(taskName);
+	for (; itr != itrOnePastTheLast; ++itr) {
+		if (itr->second == signalName) {
+			return true;	// found the specified task
+		}
+	}
+
+	// no signal found
+	return false;
+}
+
+//
+//	TODO: Write unit-tests for this method!!!
+//
+void mtsCollector::AdjustPeriod(const double newPeriod)
+{
+	CMN_ASSERT(taskManager);
+
+	double minPeriod = GetAveragePeriod();
+	double period = 0.0;
+
+	mtsTaskPeriodic * taskPeriodic = NULL;
+	SignalMap::const_iterator itrTask = SignalCollection.begin();
+
+	//SignalMap::const_iterator itrSignal, itrLast;
+	for (; itrTask != SignalCollection.end(); ++itrTask ) {
+		//
+		//	TODO: 1 signal per 1 task? or N signal per 1 task??
+		//
+		//itrLast = SignalCollection.find(itrTask->first);
+		//for (; itrSignal != itrLast; ++itrSignal ) {
+		//}
+		taskPeriodic = dynamic_cast<mtsTaskPeriodic *>(taskManager->GetTask(itrTask->first));
+		if (taskPeriodic) {
+			period = taskPeriodic->GetPeriodicity();
+		} else {
+			period = taskPeriodic->GetAveragePeriod();
+		}		
+
+		if (minPeriod > period) {
+			minPeriod = period;
+		}
+	}
+
+	minPeriod *= 0.5;
+	
+	//
+	//	TODO: How to control the period of the collector task automatically?
+	//
+
+	CMN_LOG_CLASS(5) << "Signal period updated: " << minPeriod << std::endl;
 }
--- branches/branches/2009-02-25_data_collection/libs/include/cisstMultiTask/mtsCollector.h
+++ branches/branches/2009-02-25_data_collection/libs/include/cisstMultiTask/mtsCollector.h
@@ -2,7 +2,7 @@
 /* ex: set filetype=cpp softtabstop=4 shiftwidth=4 tabstop=4 cindent expandtab: */
 
 /*
-  $Id: $
+  $Id: mtsCollector.h 2009-03-02 mjung5
 
   Author(s):  Min Yang Jung
   Created on: 2009-02-25
@@ -28,42 +28,65 @@ http://www.cisst.org/cisst/license.txt.
 #ifndef _mtsCollector_h
 #define _mtsCollector_h
 
+#include <cisstCommon/cmnUnits.h>
 #include <cisstMultiTask/mtsTaskPeriodic.h>
-
 #include <cisstMultiTask/mtsExport.h>
+#include <cisstMultiTask/mtsTaskManager.h>
 
 #include <string>
+#include <map>
+
+// enable this macro for unit-test purposes only
+#define	_OPEN_PRIVATE_FOR_UNIT_TEST_
 
 /*!
   \ingroup cisstMultiTask
-o
+
   This class provides a way to collect data from state table in real-time.
   Collected data can be either saved as a log file or displayed in GUI like an oscilloscope.
 */
 class CISST_EXPORT mtsCollector : public mtsTaskPeriodic
 {
+	CMN_DECLARE_SERVICES(CMN_NO_DYNAMIC_CREATION, 5);
+
+#ifdef _OPEN_PRIVATE_FOR_UNIT_TEST_
+public:
+#else
 private:
+#endif
+	typedef std::multimap<std::string, std::string> SignalMap;	// (taskName, signalName)
+	SignalMap SignalCollection;
+
 	static unsigned int CollectorCount;
+	static mtsTaskManager * taskManager;
 
 public:
-	mtsCollector(const std::string & collectorName, double period);
+	mtsCollector(const std::string & collectorName, double period = 100 * cmn_ms);
 	virtual ~mtsCollector();
 
 	//------------ Thread management functions (called internally) -----------//
-	/* set some initial values */
+	/*! set some initial values */
 	void Startup(void);
 
-	/* performed periodically */
+	/*! performed periodically */
     void Run(void);
 
-	/* clean-up */
+	/*! clean-up */
     void Cleanup(void);
 
 	//----------------- Signal registration for collection ------------------//
-	/* Add signal */
-	bool AddSignal(const std::string & taskName, 
-				   const std::string & signalName, 
-				   const std::string & format);
+	/*! Add a signal to the list */
+	bool AddSignal(const std::string & taskName, const std::string & signalName, 
+				   const std::string & format);	// format is currently meaningless.
+
+	/*! Remove a signal from the list */
+	bool RemoveSignal(const std::string & taskName, const std::string & signalName);
+
+	/*! Find a signal from the list */
+	bool FindSignal(const std::string & taskName, const std::string & signalName);
+
+	/*! Adjust the period of the collector task automatically */
+	void AdjustPeriod(const double newPeriod);
 
 	//
 	// To be considered more or to be implemented
@@ -75,7 +98,7 @@ public:
 	//void SetFormatInterpreter(callback);
 
 	//---------------------- Miscellaneous functions ------------------------//
-	inline const unsigned int GetCollectorCount() const { return CollectorCount; }
+	inline static const unsigned int GetCollectorCount() { return CollectorCount; }
 
 protected:
 
