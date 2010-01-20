5dd57eb0a7ea5ca9f89ae55b4ec5510d916488f9
Anton Deguet <anton.deguet@jhu.edu>
2010-01-20 21:58:03 +0000

    branches/vctTypemaps: added tests for mtsGenericObjectProxy, mtsVector and mtsMatrix
    git-svn-id: https://svn.lcsr.jhu.edu/cisst@1066 0959eb1a-6956-4f60-947b-6092a4e22c97

--- branches/branches/2009-04-17-vctTypemaps/tests/wrapping/cisstMultiTask/mtsGenericObjectProxyTest.py
+++ branches/branches/2009-04-17-vctTypemaps/tests/wrapping/cisstMultiTask/mtsGenericObjectProxyTest.py
@@ -20,51 +20,58 @@
 # --- end cisst license ---
 
 import unittest
-import numpy
 
-import cisstVectorPython
-from cisstVectorPython import vctFrm3
+import cisstCommonPython
+import cisstMultiTaskPython
 
-class FrameTest(unittest.TestCase):
+class GenericObjectProxyTest(unittest.TestCase):
     def setUp(self):
         """Call before every test case."""
-        self.ZeroTranslation = numpy.zeros(3, numpy.float)
-        self.IdentityRotation = numpy.zeros((3, 3), numpy.float)
-        self.IdentityRotation[0, 0] = 1.0
-        self.IdentityRotation[1, 1] = 1.0
-        self.IdentityRotation[2, 2] = 1.0
 
     def tearDown(self):
         """Call after every test case."""
         
-    def TestCreation(self):
-        """Test default constructor."""
-        frame = vctFrm3()
-        translation = frame.Translation()
-        translationIsZero = (self.ZeroTranslation == translation).all()
-        self.failUnless(translationIsZero)
-        rotation = frame.Rotation()
-        rotationIsIdentity = (self.IdentityRotation == rotation).all()
-        self.failUnless(rotationIsIdentity)
+    def Test_mtsInt(self):
+        """Test mtsInt"""
+        variable = cisstMultiTaskPython.mtsInt(4)
+        # check type
+        self.failUnless(isinstance(variable, cisstMultiTaskPython.mtsInt))
+        self.failUnless(isinstance(variable, cisstMultiTaskPython.mtsGenericObject))
+        self.failUnless(isinstance(variable, cisstCommonPython.cmnGenericObject))
+        # check default value and modify
+        self.failUnless(variable.Data == 4)
+        variable.Data = 8
+        self.failUnless(variable.Data == 8)
 
-    def TestTranslationModification(self):
-        """Test translation modification."""
-        frame = vctFrm3()
-        translation = frame.Translation()
-        translation.fill(10.0)
-        translationOtherRef = frame.Translation()
-        translationsAreEqual = (translation == translationOtherRef).all()
-        self.failUnless(translationsAreEqual)
+    def Test_mtsDouble(self):
+        """Test mtsDouble"""
+        variable = cisstMultiTaskPython.mtsDouble(4.0)
+        # check type
+        self.failUnless(isinstance(variable, cisstMultiTaskPython.mtsDouble))
+        self.failUnless(isinstance(variable, cisstMultiTaskPython.mtsGenericObject))
+        self.failUnless(isinstance(variable, cisstCommonPython.cmnGenericObject))
+        # check default value and modify
+        self.failUnless(variable.Data == 4.0)
+        variable.Data = 8.0
+        self.failUnless(variable.Data == 8.0)
 
-    def TestRotationModification(self):
-        """Test translation modification."""
-        frame = vctFrm3()
-        rotation = frame.Rotation()
-        rotation[0, 0] = 0.0
-        rotation[0, 1] = -1.0
-        rotation[1, 0] = 1.0
-        rotation[1, 1] = 0.0
-        rotationOtherRef = frame.Rotation()
-        rotationsAreEqual = (rotation == rotationOtherRef).all()
-        self.failUnless(rotationsAreEqual)
-        self.failUnless(numpy.linalg.det(rotation) == 1.0)
+    def Test_mtsGenericObject(self):
+        """Test mtsGenericObject methods"""
+        variable = cisstMultiTaskPython.mtsInt(4)
+        # check type
+        self.failUnless(isinstance(variable, cisstMultiTaskPython.mtsInt))
+        self.failUnless(isinstance(variable, cisstMultiTaskPython.mtsGenericObject))
+        # check default timestamp and modify
+        timestamp = variable.GetTimestamp()
+        self.failUnless(timestamp == 0.0)
+        variable.SetTimestamp(10.0)
+        timestamp = variable.GetTimestamp()
+        self.failUnless(timestamp == 10.0)
+        # check default valid flag and modify
+        valid = variable.GetValid()
+        self.failUnless(valid == False)
+        variable.SetValid(True)
+        valid = variable.GetValid()
+        self.failUnless(valid == True)
+        
+
