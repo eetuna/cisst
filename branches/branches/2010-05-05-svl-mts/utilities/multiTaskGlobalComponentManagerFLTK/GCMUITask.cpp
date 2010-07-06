85be62bad672f455ee123b4e25e5ac130fcc77c8
Min Yang Jung <myj@jhu.edu>
2010-07-06 01:44:43 +0000

    branches/svl-mts: minor update on GCM FLTK
    git-svn-id: https://svn.lcsr.jhu.edu/cisst@1607 0959eb1a-6956-4f60-947b-6092a4e22c97

--- branches/branches/2010-05-05-svl-mts/utilities/multiTaskGlobalComponentManagerFLTK/GCMUITask.cpp
+++ branches/branches/2010-05-05-svl-mts/utilities/multiTaskGlobalComponentManagerFLTK/GCMUITask.cpp
@@ -701,7 +701,7 @@ void GCMUITask::PopulateProvidedInterfaces(const std::string & processName, cons
 
     // Get all names of provided interfaces in the component
     StringVector names;
-    GlobalComponentManager.GetNamesOfProvidedInterfaces(processName, componentName, names);
+    GlobalComponentManager.GetNamesOfInterfacesProvided(processName, componentName, names);
 
     for (StringVector::size_type i = 0; i < names.size(); ++i) {
         AddLineToBrowser(UI.BrowserProvidedInterfaces, names[i].c_str());
@@ -740,7 +740,7 @@ void GCMUITask::PopulateRequiredInterfaces(const std::string & processName, cons
 
     // Get all names of provided interfaces in the component
     StringVector names;
-    GlobalComponentManager.GetNamesOfInterfaceRequired(processName, componentName, names);
+    GlobalComponentManager.GetNamesOfInterfacesRequired(processName, componentName, names);
 
     for (StringVector::size_type i = 0; i < names.size(); ++i) {
         AddLineToBrowser(UI.BrowserRequiredInterfaces, names[i].c_str());
