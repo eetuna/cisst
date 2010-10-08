4eef3b4ec061ab5bc42b587cf8f77425d3b0b09c
Balazs Vagvolgyi <balazs.vagvolgyi@jhu.edu>
2010-10-08 21:22:13 +0000

    git-svn-id: https://svn.lcsr.jhu.edu/cisst@1861 0959eb1a-6956-4f60-947b-6092a4e22c97

--- branches/branches/2010-08-16-mts-manager/libs/include/cisstStereoVision/svlSampleImageTypes.h
+++ branches/branches/2010-08-16-mts-manager/libs/include/cisstStereoVision/svlSampleImageTypes.h
@@ -23,6 +23,7 @@ http://www.cisst.org/cisst/license.txt.
 #ifndef _svlSampleImageTypes_h
 #define _svlSampleImageTypes_h
 
+#include <cisstMultitask/mtsGenericObjectProxy.h>
 #include <cisstStereoVision/svlSampleImageCustom.h>
 
 // Always include last!
@@ -37,7 +38,6 @@ typedef svlSampleImageCustom<unsigned char,  3, 1>   svlSampleImageRGB;
 typedef svlSampleImageCustom<unsigned char,  4, 1>   svlSampleImageRGBA;
 typedef svlSampleImageCustom<unsigned char,  3, 2>   svlSampleImageRGBStereo;
 typedef svlSampleImageCustom<unsigned char,  4, 2>   svlSampleImageRGBAStereo;
-//typedef svlSampleImageCustom<float,          1, 1>   svlSampleImageMonoFloat;
 typedef svlSampleImageCustom<float,          3, 1>   svlSampleImage3DMap;
 
 CMN_DECLARE_SERVICES_INSTANTIATION_EXPORT(svlSampleImageMono8)
@@ -48,8 +48,26 @@ CMN_DECLARE_SERVICES_INSTANTIATION_EXPORT(svlSampleImageRGB)
 CMN_DECLARE_SERVICES_INSTANTIATION_EXPORT(svlSampleImageRGBA)
 CMN_DECLARE_SERVICES_INSTANTIATION_EXPORT(svlSampleImageRGBStereo)
 CMN_DECLARE_SERVICES_INSTANTIATION_EXPORT(svlSampleImageRGBAStereo)
-//CMN_DECLARE_SERVICES_INSTANTIATION_EXPORT(svlSampleImageMonoFloat)
 CMN_DECLARE_SERVICES_INSTANTIATION_EXPORT(svlSampleImage3DMap)
+
+typedef mtsGenericObjectProxy<svlSampleImageMono8> svlSampleImageMono8Proxy;
+CMN_DECLARE_SERVICES_INSTANTIATION_EXPORT(svlSampleImageMono8Proxy);
+typedef mtsGenericObjectProxy<svlSampleImageMono8Stereo> svlSampleImageMono8StereoProxy;
+CMN_DECLARE_SERVICES_INSTANTIATION_EXPORT(svlSampleImageMono8StereoProxy);
+typedef mtsGenericObjectProxy<svlSampleImageMono16> svlSampleImageMono16Proxy;
+CMN_DECLARE_SERVICES_INSTANTIATION_EXPORT(svlSampleImageMono16Proxy);
+typedef mtsGenericObjectProxy<svlSampleImageMono16Stereo> svlSampleImageMono16StereoProxy;
+CMN_DECLARE_SERVICES_INSTANTIATION_EXPORT(svlSampleImageMono16StereoProxy);
+typedef mtsGenericObjectProxy<svlSampleImageRGB> svlSampleImageRGBProxy;
+CMN_DECLARE_SERVICES_INSTANTIATION_EXPORT(svlSampleImageRGBProxy);
+typedef mtsGenericObjectProxy<svlSampleImageRGBA> svlSampleImageRGBAProxy;
+CMN_DECLARE_SERVICES_INSTANTIATION_EXPORT(svlSampleImageRGBAProxy);
+typedef mtsGenericObjectProxy<svlSampleImageRGBStereo> svlSampleImageRGBStereoProxy;
+CMN_DECLARE_SERVICES_INSTANTIATION_EXPORT(svlSampleImageRGBStereoProxy);
+typedef mtsGenericObjectProxy<svlSampleImageRGBAStereo> svlSampleImageRGBAStereoProxy;
+CMN_DECLARE_SERVICES_INSTANTIATION_EXPORT(svlSampleImageRGBAStereoProxy);
+typedef mtsGenericObjectProxy<svlSampleImage3DMap> svlSampleImage3DMapProxy;
+CMN_DECLARE_SERVICES_INSTANTIATION_EXPORT(svlSampleImage3DMapProxy);
 
 #endif // _svlSampleImageTypes_h
 
