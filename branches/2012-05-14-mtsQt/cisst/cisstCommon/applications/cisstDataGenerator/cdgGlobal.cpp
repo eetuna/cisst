613a6356203b6a08f8a019b581dcc485c33f4a21
Praneeth Sadda <psadda1@jhu.edu>
2012-05-14 15:56:53 +0000

    Branch for work on mtsQt
    git-svn-id: https://svn.lcsr.jhu.edu/cisst@3632 0959eb1a-6956-4f60-947b-6092a4e22c97

---
+++ branches/2012-05-14-mtsQt/cisst/cisstCommon/applications/cisstDataGenerator/cdgGlobal.cpp
@@ -0,0 +1,108 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-    */
+/* ex: set filetype=cpp softtabstop=4 shiftwidth=4 tabstop=4 cindent expandtab: */
+
+/*
+  $Id$
+
+  Author(s):  Anton Deguet
+  Created on: 2010-09-06
+
+  (C) Copyright 2010-2012 Johns Hopkins University (JHU), All Rights
+  Reserved.
+
+  --- begin cisst license - do not edit ---
+
+  This software is provided "as is" under an open source license, with
+  no warranty.  The complete license can be found in license.txt and
+  http://www.cisst.org/cisst/license.txt.
+
+  --- end cisst license ---
+
+*/
+
+#include "cdgGlobal.h"
+
+CMN_IMPLEMENT_SERVICES(cdgGlobal);
+
+
+cdgGlobal::cdgGlobal(unsigned int lineNumber):
+    cdgScope(lineNumber)
+{}
+
+
+cdgScope::Type cdgGlobal::GetScope(void) const
+{
+    return cdgScope::CDG_GLOBAL;
+}
+
+
+bool cdgGlobal::HasKeyword(const std::string & keyword) const
+{
+    if ((keyword == "name")
+        || (keyword == "include")) {
+        return true;
+    }
+    return false;
+}
+
+
+bool cdgGlobal::HasScope(const std::string & keyword,
+                         cdgScope::Stack & scopes,
+                         unsigned int lineNumber)
+{
+    if (keyword == "class") {
+        cdgClass * newClass = new cdgClass(lineNumber);
+        scopes.push_back(newClass);
+        Scopes.push_back(newClass);
+        Classes.push_back(newClass);
+        return true;
+    } else if (keyword == "inline-header") {
+        cdgInline * newCode = new cdgInline(lineNumber, cdgInline::CDG_INLINE_HEADER);
+        scopes.push_back(newCode);
+        Scopes.push_back(newCode);
+        return true;
+    } else if (keyword == "inline-code") {
+        cdgInline * newCode = new cdgInline(lineNumber, cdgInline::CDG_INLINE_CODE);
+        scopes.push_back(newCode);
+        Scopes.push_back(newCode);
+        return true;
+    }
+    return false;
+}
+
+
+bool cdgGlobal::SetValue(const std::string & keyword,
+                         const std::string & CMN_UNUSED(value),
+                         std::string & errorMessage)
+{
+    errorMessage = "unhandled keyword \"" + keyword + "\"";
+    return false;
+}
+
+
+bool cdgGlobal::IsValid(std::string & CMN_UNUSED(errorMessage)) const
+{
+    return true;
+}
+
+
+void cdgGlobal::GenerateHeader(std::ostream & outputStream) const
+{
+    size_t index;
+    GenerateLineComment(outputStream);
+    for (index = 0; index < Scopes.size(); index++) {
+        Scopes[index]->GenerateHeader(outputStream);
+        outputStream << std::endl;
+    }
+}
+
+
+void cdgGlobal::GenerateCode(std::ostream & outputStream) const
+{
+    size_t index;
+    GenerateLineComment(outputStream);
+    for (index = 0; index < Scopes.size(); index++) {
+        Scopes[index]->GenerateCode(outputStream);
+        outputStream << std::endl;
+    }
+}
---
+++ branches/2012-05-14-mtsQt/cisst/cisstVector/vctQtWidgetDynamicVector.h
@@ -0,0 +1,89 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-    */
+/* ex: set filetype=cpp softtabstop=4 shiftwidth=4 tabstop=4 cindent expandtab: */
+
+/*
+  $Id$
+
+  Author(s):  Anton Deguet
+  Created on: 2011-12-08
+
+  (C) Copyright 2011-2012 Johns Hopkins University (JHU), All Rights
+  Reserved.
+
+--- begin cisst license - do not edit ---
+
+This software is provided "as is" under an open source license, with
+no warranty.  The complete license can be found in license.txt and
+http://www.cisst.org/cisst/license.txt.
+
+--- end cisst license ---
+
+*/
+
+#ifndef _vctQtWidgetDynamicVector_h
+#define _vctQtWidgetDynamicVector_h
+
+#include <QTableWidget>
+
+#include <cisstVector/vctDynamicVector.h>
+
+// Always include last
+#include <cisstVector/vctExportQt.h>
+
+
+template <class _elementType>
+class vctQtWidgetDynamicVectorRead
+{
+    typedef _elementType value_type;
+    QTableWidget * Table;
+
+ public:
+    vctQtWidgetDynamicVectorRead(void);
+    QWidget * GetWidget(void);
+    virtual bool SetValue(const vctDynamicVector<value_type> & value);
+};
+
+typedef vctQtWidgetDynamicVectorRead<double> vctQtWidgetDynamicVectorDoubleRead;
+typedef vctQtWidgetDynamicVectorRead<float> vctQtWidgetDynamicVectorFloatRead;
+typedef vctQtWidgetDynamicVectorRead<int> vctQtWidgetDynamicVectorIntRead;
+typedef vctQtWidgetDynamicVectorRead<unsigned int> vctQtWidgetDynamicVectorUIntRead;
+typedef vctQtWidgetDynamicVectorRead<bool> vctQtWidgetDynamicVectorBoolRead;
+
+#if (CISST_OS == CISST_WINDOWS)
+template class CISST_EXPORT vctQtWidgetDynamicVectorRead<double>;
+template class CISST_EXPORT vctQtWidgetDynamicVectorRead<float>;
+template class CISST_EXPORT vctQtWidgetDynamicVectorRead<int>;
+template class CISST_EXPORT vctQtWidgetDynamicVectorRead<unsigned int>;
+template class CISST_EXPORT vctQtWidgetDynamicVectorRead<bool>;
+#endif // CISST_WINDOWS
+
+// -- for doubles
+class CISST_EXPORT vctQtWidgetDynamicVectorDoubleWrite: public QTableWidget
+{
+ public:
+    vctQtWidgetDynamicVectorDoubleWrite(void);
+    virtual bool SetValue(const vctDynamicVector<double> & value);
+    virtual bool GetValue(vctDynamicVector<double> & placeHolder) const;
+};
+
+
+// -- for ints
+class CISST_EXPORT vctQtWidgetDynamicVectorIntWrite: public QTableWidget
+{
+ public:
+    vctQtWidgetDynamicVectorIntWrite(void);
+    virtual bool SetValue(const vctDynamicVector<int> & value);
+    virtual bool GetValue(vctDynamicVector<int> & placeHolder) const;
+};
+
+
+// -- for bools
+class CISST_EXPORT vctQtWidgetDynamicVectorBoolWrite: public QTableWidget
+{
+ public:
+    vctQtWidgetDynamicVectorBoolWrite(void);
+    virtual bool SetValue(const vctDynamicVector<bool> & value);
+    virtual bool GetValue(vctDynamicVector<bool> & placeHolder) const;
+};
+
+#endif // _vctQtWidgetDynamicVector_h
